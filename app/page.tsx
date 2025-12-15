e816b5723d3052544f400d77faf7f83b865ae24a
walnuts1018 <r.juglans.1018@gmail.com>
2025-12-15 16:51:11 +0900

    Fix request origin retrieval by removing protocol handling in header parsing
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,7 @@ async function getRequestOriginFromHeaders(): Promise<string> {
   const h = await headers();
   const host = h.get("x-forwarded-host") ?? h.get("host");
   if (!host) return "";
-
-  const proto = h.get("x-forwarded-proto") ?? "https";
-  return `${proto}://${host}`;
+  return `https://${host}`;
 }
 
 export default async function Home() {
